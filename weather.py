23756a19d692cc6ab3a2464622e36f0dc0beb4c5
DorianMabrey <51933248+DorianMabrey@users.noreply.github.com>
2021-09-20 13:43:40 -0400

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
 import requests
 
 # API url
-url = 'http://api.openweathermap.org/data/2.5/weather?zip=21230,&appid=ca4edf7fd84d13a4f8a978b452bfa425'
+url = 'http://api.openweathermap.org/data/2.5/weather?zip=21230,&appid={API_KEY}'
 # GET request to Open Weather
 response = requests.get(url)
 # Print content in JSON
